03bbeaaf5bef221a3370f88ccb7a1f1f7dde5ce3
Franklin Shera <fshera96@gmail.com>
2023-04-11 07:35:46 +0300

    dashboard url const

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,7 @@
 export const APP_URL = import.meta.env.APP_URL || "http://localhost:3000";
 
+export const DASHBOARD_HOME = "/dashboard";
+
 // github oauth
 export const GITHUB_CLIENT_ID = import.meta.env.GITHUB_CLIENT_ID;
 export const GITHUB_CLIENT_SECRET = import.meta.env.GITHUB_CLIENT_SECRET;
