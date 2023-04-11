807dd6ac7e23159d3dcd75512060c1c8e4c5a476
Franklin Shera <fshera96@gmail.com>
2023-04-11 06:18:28 +0300

    github

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,5 @@
 export const APP_URL = import.meta.env.APP_URL || "http://localhost:3000";
+
+// github oauth
+export const GITHUB_CLIENT_ID = import.meta.env.GITHUB_CLIENT_ID;
+export const GITHUB_CLIENT_SECRET = import.meta.env.GITHUB_CLIENT_SECRET;
